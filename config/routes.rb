3f8588caabb94aa2e3d47f1e7a99465b793c1c5f
Igor Petrovski <ipetrovski3@gmail.com>
2020-06-18 18:12:14 +0200

    User model validations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :users
+
   root to: 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
