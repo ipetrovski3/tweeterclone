b0d48cfa6d85c93a55a0f8905d4a032478bedc70
ipetrovski3 <ipetrovski3@gmail.com>
2020-07-30 20:28:57 +0200

    error fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
